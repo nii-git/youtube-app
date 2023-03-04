d27d2eaa882f3d3c407a0001101a433bf886a3f2
nii-git <murasa-nui1@gmail.com>
2023-03-04 13:42:57 +0900

    WIP: 再生リスト取得処理

--- api/main.go
+++ api/main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type config struct {
+	YoutubeApiKey string `json:youtubeApiKey`
+}
+type apiResponse struct {
+	Kind string `json:kind`
+}
 
 func main() {
-	fmt.Println("Hello,world!")
+	var youtubeListId string
+	var config config
+
+	// 引数チェック
+	if len(os.Args) != 2 {
+		fmt.Println("Invalid args")
+		return
+	} else {
+		youtubeListId = os.Args[1]
+	}
+
+	getPlayListAPIUrl := "https://www.googleapis.com/youtube/v3/playlistItems"
+	// 設定ファイル読み込み
+	file, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	resp, err := http.Get(getPlayListAPIUrl + "?key=" + config.YoutubeApiKey + "&part=snippet&playlistId=" + youtubeListId)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if resp.StatusCode != 200 {
+		fmt.Println("ERROR: StatusCode:" + strconv.Itoa(resp.StatusCode))
+		return
+	}
+
+	//fmt.Println("url:" + getPlayListAPIUrl + "?key=" + config.YoutubeApiKey)
+
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	var x apiResponse
+
+	err = json.Unmarshal(byteArray, &x)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("(%%#v) %#v\n", x)
+
+	//fmt.Println(string(byteArray))
 }
+
+// https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyBsIWp3vAWFF6_UfX_4F5P5Pb7KCMcSaiM&part=snippet&playlistId=PLRdiaanKAFQl5ERDgJHx2ZRKCcIl-I8fz&maxResults=50
